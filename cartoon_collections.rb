df3b9e40f04fbbfa9203334f931314a3ff246993
Evan  Markert <evanmarkert@gmail.com>
2017-12-04 21:48:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,15 @@ def long_planeteer_calls(calls)
     calls.any? { |e| e.length > 4  }
 end
 
+#def find_the_cheese(ingredients)
+#    cheese_types = ["cheddar", "gouda", "camembert"]
+#    cheese = ingredients & cheese_types
+#    cheese.first
+#end
+
 def find_the_cheese(ingredients)
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    cheese = ingredients & cheese_types
-    cheese.first
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
